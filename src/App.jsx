c23e2065c59aab9b82a8fb221e57e333dab17835
Alexander Villamizar <33869219+javq36@users.noreply.github.com>
2022-08-13 18:43:17 -0500

    Initial pull

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>Tik Tok clone</main>
+    </div>
+  );
+}
+
+export default App;
